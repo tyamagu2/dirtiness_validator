d84c7de59bd144c2307ead8731030772075b21d6
tyamagu2 <tyamagu2@gmail.com>
2015-10-01 22:30:06 +0900

    Add basic test cases

--- spec/dirtiness_validator_spec.rb
+++ spec/dirtiness_validator_spec.rb
@@ -5,7 +5,126 @@ describe DirtinessValidator do
     expect(DirtinessValidator::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '#validate_each' do
+    let(:previous_value) { 100 }
+    let(:model) { TestModel.new(attr: previous_value).tap(&:save) }
+    subject { model.valid? }
+
+    before { model.attr = current_value }
+    after { TestModel.clear_validators! }
+
+    describe 'greater' do
+      before { TestModel.validates :attr, dirtiness: { greater: true } }
+
+      context 'when current value is greater than the previous value' do
+        let(:current_value) { 200 }
+        it { expect { subject }.not_to change { model.errors[:attr] }.from([]) }
+      end
+
+      context 'when current value is equal to the previous value' do
+        let(:current_value) { previous_value }
+        it { expect { subject }.to change { model.errors[:attr] }.from([]).to([I18n.t('errors.messages.greater')]) }
+      end
+
+      context 'when current value is less than the previous value' do
+        let(:current_value) { 50 }
+        it { expect { subject }.to change { model.errors[:attr] }.from([]).to([I18n.t('errors.messages.greater')]) }
+      end
+    end
+
+    describe 'greater_or_equal' do
+      before { TestModel.validates :attr, dirtiness: { greater_or_equal: true } }
+
+      context 'when current value is greater than the previous value' do
+        let(:current_value) { 200 }
+        it { expect { subject }.not_to change { model.errors[:attr] }.from([]) }
+      end
+
+      context 'when current value is equal to the previous value' do
+        let(:current_value) { previous_value }
+        it { expect { subject }.not_to change { model.errors[:attr] }.from([]) }
+      end
+
+      context 'when current value is less than the previous value' do
+        let(:current_value) { 50 }
+        it { expect { subject }.to change { model.errors[:attr] }.from([]).to([I18n.t('errors.messages.greater_or_equal')]) }
+      end
+    end
+
+    describe 'less' do
+      before { TestModel.validates :attr, dirtiness: { less: true } }
+
+      context 'when current value is greater than the previous value' do
+        let(:current_value) { 200 }
+        it { expect { subject }.to change { model.errors[:attr] }.from([]).to([I18n.t('errors.messages.less')]) }
+      end
+
+      context 'when current value is equal to the previous value' do
+        let(:current_value) { previous_value }
+        it { expect { subject }.to change { model.errors[:attr] }.from([]).to([I18n.t('errors.messages.less')]) }
+      end
+
+      context 'when current value is less than the previous value' do
+        let(:current_value) { 50 }
+        it { expect { subject }.not_to change { model.errors[:attr] }.from([]) }
+      end
+    end
+
+    describe 'less_or_equal' do
+      before { TestModel.validates :attr, dirtiness: { less_or_equal: true } }
+
+      context 'when current value is greater than the previous value' do
+        let(:current_value) { 200 }
+        it { expect { subject }.to change { model.errors[:attr] }.from([]).to([I18n.t('errors.messages.less_or_equal')]) }
+      end
+
+      context 'when current value is equal to the previous value' do
+        let(:current_value) { previous_value }
+        it { expect { subject }.not_to change { model.errors[:attr] }.from([]) }
+      end
+
+      context 'when current value is less than the previous value' do
+        let(:current_value) { 50 }
+        it { expect { subject }.not_to change { model.errors[:attr] }.from([]) }
+      end
+    end
+
+    describe 'equal' do
+      before { TestModel.validates :attr, dirtiness: { equal: true } }
+
+      context 'when current value is greater than the previous value' do
+        let(:current_value) { 200 }
+        it { expect { subject }.to change { model.errors[:attr] }.from([]).to([I18n.t('errors.messages.equal')]) }
+      end
+
+      context 'when current value is equal to the previous value' do
+        let(:current_value) { previous_value }
+        it { expect { subject }.not_to change { model.errors[:attr] }.from([]) }
+      end
+
+      context 'when current value is less than the previous value' do
+        let(:current_value) { 50 }
+        it { expect { subject }.to change { model.errors[:attr] }.from([]).to([I18n.t('errors.messages.equal')]) }
+      end
+    end
+
+    describe 'not_equal' do
+      before { TestModel.validates :attr, dirtiness: { not_equal: true } }
+
+      context 'when current value is greater than the previous value' do
+        let(:current_value) { 200 }
+        it { expect { subject }.not_to change { model.errors[:attr] }.from([]) }
+      end
+
+      context 'when current value is equal to the previous value' do
+        let(:current_value) { previous_value }
+        it { expect { subject }.to change { model.errors[:attr] }.from([]).to([I18n.t('errors.messages.not_equal')]) }
+      end
+
+      context 'when current value is less than the previous value' do
+        let(:current_value) { 50 }
+        it { expect { subject }.not_to change { model.errors[:attr] }.from([]) }
+      end
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,31 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'dirtiness_validator'
+
+I18n.load_path += Dir[File.expand_path(File.join(File.dirname(__FILE__), '../locales', '*.yml')).to_s]
+
+class TestModel
+  include ActiveModel::Model
+  include ActiveModel::Validations
+  include ActiveModel::Dirty
+
+  define_attribute_methods :attr
+
+  def attr
+    @attr
+  end
+
+  def attr=(val)
+    return if @attr == val
+
+    attr_will_change!
+    @attr = val
+  end
+
+  def save
+    changes_applied
+  end
+
+  def reload!
+    clear_changes_information
+  end
+end
