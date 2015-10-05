c72970410000034575e16668ce5ddfea54a8b886
tyamagu2 <tyamagu2@gmail.com>
2015-10-05 22:51:06 +0900

    Autoload locale file on load

--- lib/dirtiness_validator.rb
+++ lib/dirtiness_validator.rb
@@ -3,3 +3,7 @@ require 'active_model/validations/dirtiness_validator'
 
 module DirtinessValidator
 end
+
+ActiveSupport.on_load(:i18n) do
+  I18n.load_path += Dir[File.expand_path(File.join(*File.dirname(__FILE__), '..', 'locales', 'en.yml'))]
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'dirtiness_validator'
 
-I18n.load_path += Dir[File.expand_path(File.join(File.dirname(__FILE__), '../locales', '*.yml')).to_s]
-
 class TestModel
   include ActiveModel::Model
   include ActiveModel::Validations
